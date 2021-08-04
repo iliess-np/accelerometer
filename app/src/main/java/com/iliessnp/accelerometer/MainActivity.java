1843d1fbc8fdb844b5cfe3b5413d388590a60a5d
iliess-np <iliessnapolion@gmail.com>
2021-08-04 15:01:36 +0100

    make  it more realistic condition to fire the event

--- app/src/main/java/com/iliessnp/accelerometer/MainActivity.java
+++ app/src/main/java/com/iliessnp/accelerometer/MainActivity.java
@@ -1,7 +1,5 @@
 package com.iliessnp.accelerometer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -16,33 +14,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private  static final String TAG = "MainActivity";
-
+    private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
     Sensor accelerometer;
-    TextView x,y,z,sum,jump,fall;
-    float xx,yy,zz,summ;
-    int falll,jumpp;
-    ArrayList<Float> xData = new ArrayList<Float>();
-    ArrayList<Float> yData = new ArrayList<Float>();
-    ArrayList<Float> zData = new ArrayList<Float>();
-
+    TextView x, y, z, sum, jump, fall;
+    float xx, yy, zz, summ;
+    int falll, jumpp;
+    ArrayList<Float> xData = new ArrayList<>();
+    ArrayList<Float> yData = new ArrayList<>();
+    ArrayList<Float> zData = new ArrayList<>();
+    ArrayList<Float> acceleration = new ArrayList<>();
+    long timeNow, timePrv = 0;
+    
     //save to file
-    String filename,filepath,filecontent;
-    Button btnSave,btnLoad;
+    String filename, filepath, filecontent;
+    Button btnSave, btnLoad;
     TextView tvLoad;
     EditText etInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,33 +54,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sum = findViewById(R.id.sum);
         jump = findViewById(R.id.jump);
         fall = findViewById(R.id.fall);
-
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
+        assert sensorManager != null;
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(MainActivity.this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        falll = Integer.parseInt(fall.getText().toString());
+        jumpp = Integer.parseInt(jump.getText().toString());
 
         //file save
-        btnSave=findViewById(R.id.btnSave);
-        btnLoad=findViewById(R.id.btnLoad);
-        etInput =findViewById(R.id.etInput);
-        tvLoad =findViewById(R.id.tvLoad);
-        filename="myFile.txt";
-        filepath="myFileDir";
-        if(!isExternalStorageAvailableForRW()){
+        btnSave = findViewById(R.id.btnSave);
+        btnLoad = findViewById(R.id.btnLoad);
+        etInput = findViewById(R.id.etInput);
+        tvLoad = findViewById(R.id.tvLoad);
+        filename = "myFile.txt";
+        filepath = "myFileDir";
+        if (!isExternalStorageAvailableForRW()) {
             btnSave.setEnabled(false);
         }
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tvLoad.setText("");
-                String dataAll ="xData =>"+ xData.toString() +"yData =>"+yData.toString()+"zData =>"+zData.toString();
-                filecontent = dataAll;
-                if (!filecontent.equals("")){
-                    File myExternalFile = new File(getExternalFilesDir(filepath),filename);
-                    FileOutputStream fos = null;
+                filecontent = "acceleration => " + acceleration;
+                if (!filecontent.equals("")) {
+                    File myExternalFile = new File(getExternalFilesDir(filepath), filename);
+                    FileOutputStream fos;
                     try {
-                        fos =  new FileOutputStream(myExternalFile);
+                        fos = new FileOutputStream(myExternalFile);
                         fos.write(filecontent.getBytes());
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     }
                     etInput.setText("");
                     Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     Toast.makeText(MainActivity.this, "Fail;  text cant be empty", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -98,13 +98,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             @Override
             public void onClick(View v) {
                 FileReader fr = null;
-                File myExternalFile = new File(getExternalFilesDir(filepath),filename);
+                File myExternalFile = new File(getExternalFilesDir(filepath), filename);
                 StringBuilder stringBuilder = new StringBuilder();
                 try {
                     fr = new FileReader(myExternalFile);
                     BufferedReader br = new BufferedReader(fr);
                     String line = br.readLine();
-                    while (line != null){
+                    while (line != null) {
                         stringBuilder.append(line).append("\n");
                         line = br.readLine();
                     }
@@ -112,26 +112,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
-                }finally {
+                } finally {
                     String fileContents = "File Content\n" + stringBuilder.toString();
                     tvLoad.setText(fileContents);
                 }
             }
         });
     }
+
     //save to file
     private boolean isExternalStorageAvailableForRW() {
         String externalState = Environment.getExternalStorageState();
-        if (externalState.equals(Environment.MEDIA_MOUNTED)){
+        if (externalState.equals(Environment.MEDIA_MOUNTED)) {
             return true;
         }
         return false;
     }
 
-    //accelometer
+    public void showZData(View view) {
+        for (float xX : xData) {
+            Log.d(TAG, "xData: " + xData);
+        }
+        for (float yY : yData) {
+            Log.d(TAG, "yData: " + yData);
+        }
+        for (float zZ : zData) {
+            Log.d(TAG, "zData: " + zData);
+        }
+    }
+
+    //accelerometer
     @Override
     public void onSensorChanged(SensorEvent event) {
-        //Log.d(TAG,"onSensorChanged: X => "+event.values[0] + " Y => "+event.values[1]+" Z => "+event.values[2]);
         xx = event.values[0];
         yy = event.values[1];
         zz = event.values[2];
@@ -139,55 +151,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         yData.add(event.values[1]);
         zData.add(event.values[2]);
 
-        float t = (float) (Math.pow(xx, 2)+ Math.pow(yy, 2)+ Math.pow(zz, 2));
+        float t = (float) (Math.pow(xx, 2) + Math.pow(yy, 2) + Math.pow(zz, 2));
         summ = (float) Math.sqrt(t);
+        timeNow = System.currentTimeMillis();
 
-//        if (summ>21){
-//            Toast.makeText(this, "you Jumped \nsumm is=> "+ summ, Toast.LENGTH_SHORT).show();
-//            jumpp =+1;
-//            jump.setText(String.valueOf(jumpp));
-//        }else if (summ<21 && summ>19){
-//            Toast.makeText(this, "you Fallen \nsumm is=> "+ summ, Toast.LENGTH_SHORT).show();
-//            falll =+1;
-//            fall.setText(String.valueOf(falll));
-//        }else {
-//            //nothing
-//        }
+        if (summ >= 65) {
+            jumpp = +1;
+            jump.setText(String.valueOf(jumpp));
+        } else if (summ > 35 && timeNow > timePrv + 20000) {
+            Toast.makeText(this, "you Fallen \nsumm is=> " + summ, Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "time now: " + timeNow + "\ntime prev: " + timePrv);
+            falll = +1;
+            fall.setText(String.valueOf(falll));
+            acceleration.add(summ);
+            timePrv = timeNow;
+        }
 
         x.setText(String.valueOf(xx));
         y.setText(String.valueOf(yy));
         z.setText(String.valueOf(zz));
         sum.setText(String.valueOf(summ));
-
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
-    public void showZData(View view) {
-        for (float xX : xData){
-            Log.d(TAG,"xData: "+xData);
-        }
-        for (float yY : yData){
-            Log.d(TAG,"yData: "+yData);
-        }
-        for (float zZ : zData){
-            Log.d(TAG,"zData: "+zData);
-        }
-    }
-//    public static void write (String filename, int[]x) throws IOException {
-//        BufferedWriter outputWriter = null;
-//        outputWriter = new BufferedWriter(new FileWriter(filename));
-//        for (int i = 0; i < x.length; i++) {
-//            // Maybe:
-//            outputWriter.write(x[i]+"");
-//            // Or:
-//            outputWriter.write(Integer.toString(x[i]);
-//            outputWriter.newLine();
-//        }
-//        outputWriter.flush();
-//        outputWriter.close();
-//    }
 }
